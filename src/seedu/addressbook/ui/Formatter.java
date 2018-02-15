21989b21374d3639cb3e79b31d56b1981280d86e
raymond511 <raymonds970@gmail.com>
2018-02-15 14:59:16 +0800

    Fixed typo errors. Removed formatting methods from textui.java Added formatter class to format messages updated imports for commands

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,107 @@
+package seedu.addressbook.ui;
+
+import seedu.addressbook.commands.CommandResult;
+
+import java.util.List;
+
+import static seedu.addressbook.common.Messages.*;
+
+public class Formatter {
+
+    /**
+     * A decorative prefix added to the beginning of lines printed by AddressBook
+     **/
+    private static final String LINE_PREFIX = "|| ";
+
+    /**
+     * A platform independent line separator.
+     */
+    private static final String LS = System.lineSeparator();
+
+    private static final String DIVIDER = "===================================================";
+
+    /**
+     * Format of indexed list item
+     */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+    /**
+     * A message prompting the user to input command
+     */
+    private static final String MESSAGE_PROMPT_COMMAND = LINE_PREFIX + "Enter command: ";
+
+    /**
+     * A message displaying the command entered by user
+     */
+    private static final String MESSAGE_COMMAND_ENTERED = "[Command entered:%1$s]";
+
+
+    /**
+     * Offset required to convert between 1-indexing and 0-indexing.
+     */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+
+
+    public static String getFeedbackToUser(CommandResult result) {
+        return result.feedbackToUser + "\n" + DIVIDER;
+    }
+
+    public static String getWelcomeMessage(String version, String storageFilePath) {
+        String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
+        String message = DIVIDER + "\n"
+                + DIVIDER + "\n"
+                + MESSAGE_WELCOME +"\n"
+                + version + "\n"
+                + MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE + "\n"
+                + storageFileInfo + "\n"
+                + DIVIDER;
+        return message;
+    }
+
+    public static String getInitFailedMessage() {
+        String message = MESSAGE_INIT_FAILED + "\n" + DIVIDER + "\n" + DIVIDER;
+        return message;
+    }
+
+    public static String getGoodbyeMessage() {
+        String message = MESSAGE_GOODBYE + "\n" + DIVIDER + "\n" + DIVIDER;
+        return message;
+    }
+
+    public static String getCommandPrompt() {
+        return MESSAGE_PROMPT_COMMAND;
+    }
+
+    public static String getCommandEnteredMessage(String fullInputLine) {
+        return String.format(MESSAGE_COMMAND_ENTERED, fullInputLine);
+    }
+
+    public static String getOutputMessage(String message) {
+        String outputMessage = LINE_PREFIX + message.replace("\n", LS + LINE_PREFIX);
+        return outputMessage;
+    }
+
+    /**
+     * Formats a list of strings as a viewable indexed list.
+     */
+    public static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+}
+
--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -21,21 +21,6 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
  */
 public class TextUi {
 
-    /** A decorative prefix added to the beginning of lines printed by AddressBook */
-    private static final String LINE_PREFIX = "|| ";
-
-    /** A platform independent line separator. */
-    private static final String LS = System.lineSeparator();
-
-    private static final String DIVIDER = "===================================================";
-
-    /** Format of indexed list item */
-    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
-
-
-    /** Offset required to convert between 1-indexing and 0-indexing.  */
-    public static final int DISPLAYED_INDEX_OFFSET = 1;
-
     /** Format of a comment input line. Comment lines are silently consumed when reading user input. */
     private static final String COMMENT_LINE_FORMAT_REGEX = "#.*";
 
@@ -49,6 +34,26 @@ public class TextUi {
     public TextUi(InputStream in, PrintStream out) {
         this.in = new Scanner(in);
         this.out = out;
+    }
+
+    public void showWelcomeMessage(String version, String storageFilePath) {
+        showToUser(Formatter.getWelcomeMessage(version, storageFilePath));
+    }
+
+    public void showGoodbyeMessage() {
+        showToUser(Formatter.getGoodbyeMessage());
+    }
+
+
+    public void showInitFailedMessage() {
+        showToUser(Formatter.getInitFailedMessage());
+    }
+
+    /** Shows message(s) to the user */
+    public void showToUser(String... message) {
+        for (String m : message) {
+            out.println(Formatter.getOutputMessage(m));
+        }
     }
 
     /**
@@ -79,7 +84,7 @@ public class TextUi {
      * @return command (full line) entered by the user
      */
     public String getUserCommand() {
-        out.print(LINE_PREFIX + "Enter command: ");
+        out.print(Formatter.getCommandPrompt());
         String fullInputLine = in.nextLine();
 
         // silently consume all ignored lines
@@ -87,37 +92,8 @@ public class TextUi {
             fullInputLine = in.nextLine();
         }
 
-        showToUser("[Command entered:" + fullInputLine + "]");
+        showToUser(Formatter.getCommandEnteredMessage(fullInputLine));
         return fullInputLine;
-    }
-
-
-    public void showWelcomeMessage(String version, String storageFilePath) {
-        String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
-        showToUser(
-                DIVIDER,
-                DIVIDER,
-                MESSAGE_WELCOME,
-                version,
-                MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE,
-                storageFileInfo,
-                DIVIDER);
-    }
-
-    public void showGoodbyeMessage() {
-        showToUser(MESSAGE_GOODBYE, DIVIDER, DIVIDER);
-    }
-
-
-    public void showInitFailedMessage() {
-        showToUser(MESSAGE_INIT_FAILED, DIVIDER, DIVIDER);
-    }
-
-    /** Shows message(s) to the user */
-    public void showToUser(String... message) {
-        for (String m : message) {
-            out.println(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX));
-        }
     }
 
     /**
@@ -129,7 +105,7 @@ public class TextUi {
         if (resultPersons.isPresent()) {
             showPersonListView(resultPersons.get());
         }
-        showToUser(result.feedbackToUser, DIVIDER);
+        showToUser(Formatter.getFeedbackToUser(result));
     }
 
     /**
@@ -146,27 +122,7 @@ public class TextUi {
 
     /** Shows a list of strings to the user, formatted as an indexed list. */
     private void showToUserAsIndexedList(List<String> list) {
-        showToUser(getIndexedListForViewing(list));
-    }
-
-    /** Formats a list of strings as a viewable indexed list. */
-    private static String getIndexedListForViewing(List<String> listItems) {
-        final StringBuilder formatted = new StringBuilder();
-        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
-        for (String listItem : listItems) {
-            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
-            displayIndex++;
-        }
-        return formatted.toString();
-    }
-
-    /**
-     * Formats a string as a viewable indexed list item.
-     *
-     * @param visibleIndex visible index for this listing
-     */
-    private static String getIndexedListItem(int visibleIndex, String listItem) {
-        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+        showToUser(Formatter.getIndexedListForViewing(list));
     }
 
 }
